38c9b102e8d8b6a40930b0973b926fa494088e09
Shreeja042-sai <patlollashreeja17@gmail.com>
2025-08-17 06:37:27 +0530

    Update SonarQube.sh

--- scripts/SonarQube.sh
+++ scripts/SonarQube.sh
@@ -24,3 +24,6 @@ sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Run SonarQube container in detached mode with port mapping
+# docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
