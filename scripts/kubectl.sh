fc99c3f045d66a2f1c497f9528e364fc3148422f
Shreeja042-sai <patlollashreeja17@gmail.com>
2025-08-17 07:49:55 +0530

    Update kubectl.sh

--- scripts/kubectl.sh
+++ scripts/kubectl.sh
@@ -1,3 +1,4 @@
+```bash 
 #!/bin/bash
 
 # Master Node
@@ -37,7 +38,35 @@ kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 # Deploy Ingress Controller (NGINX)
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.49.0/deploy/static/provider/baremetal/deploy.yaml
 
-```bash
-# Worker Node
+```
+
+```bash 
 
 # Worker Node
+
+#!/bin/bash
+
+# Update package manager repositories
+sudo apt-get update
+
+#Install Docker
+sudo apt install docker.io -y
+sudo chmod 666 /var/run/docker.sock
+
+# Install necessary dependencies
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
+sudo mkdir -p -m 755 /etc/apt/keyrings
+
+# Add Kubernetes Repository and GPG Key
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.33/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.33/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# Update Package List
+sudo apt update
+
+# Install Kubernetes Components
+sudo apt install -y kubeadm=1.28.1-1.1 kubelet=1.28.1-1.1 kubectl=1.28.1-1.1
+
+```
+
+
